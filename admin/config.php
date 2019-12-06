b887c8ab71c61d1f2e3b001ba919ff2db6dcfe05
serii <seriiburduja@gmail.com>
2019-12-06 14:28:47 +0200

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc-resale.host1670806.hostland.pro/admin/');
+define('HTTP_CATALOG', 'http://oc-resale.host1670806.hostland.pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc-resale.host1670806.hostland.pro/admin/');
+define('HTTPS_CATALOG', 'http://oc-resale.host1670806.hostland.pro/');
+
+// DIR
+define('DIR_APPLICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/admin/');
+define('DIR_SYSTEM', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/');
+define('DIR_IMAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/image/');
+define('DIR_LANGUAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/admin/language/');
+define('DIR_TEMPLATE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/admin/view/template/');
+define('DIR_CONFIG', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/config/');
+define('DIR_CACHE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/download/');
+define('DIR_LOGS', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/modification/');
+define('DIR_UPLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/upload/');
+define('DIR_CATALOG', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'host1670806_0106');
+define('DB_PASSWORD', 'OT6zBw6k');
+define('DB_DATABASE', 'host1670806_resale');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://oc-resale.host1670806.hostland.pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://oc-resale.host1670806.hostland.pro/');
+
+// DIR
+define('DIR_APPLICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/catalog/');
+define('DIR_SYSTEM', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/');
+define('DIR_IMAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/image/');
+define('DIR_LANGUAGE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/catalog/language/');
+define('DIR_TEMPLATE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/catalog/view/theme/');
+define('DIR_CONFIG', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/config/');
+define('DIR_CACHE', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/download/');
+define('DIR_LOGS', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/modification/');
+define('DIR_UPLOAD', '/home/host1670806/host1670806.hostland.pro/htdocs/oc-resale/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'host1670806_0106');
+define('DB_PASSWORD', 'OT6zBw6k');
+define('DB_DATABASE', 'host1670806_resale');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
